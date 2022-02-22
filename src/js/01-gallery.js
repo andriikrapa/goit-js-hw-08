b749e85d4804d9ce54b405f117f3f706fee6c61b
andriikrapa <andrii.krapa@gmail.com>
2022-02-22 23:06:23 +0100

    Add SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const GalleryCardsMurkup = createGalleryCardsMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", GalleryCardsMurkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({preview, original, description, }) => {
+            return `
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+                `;
+        })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
