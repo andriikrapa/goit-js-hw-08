355ce47f8d37b469b8f1c817b8b01e2935ed79fa
andriikrapa <andrii.krapa@gmail.com>
2022-02-23 22:32:21 +0100

    Add savePlayerCurrentTime function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,20 @@
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate', throttle(savePlayerCurrentTime, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+function savePlayerCurrentTime(e) {
+    localStorage.setItem("videoplayer-current-time", e.seconds);
+    console.log(e.seconds);
+};
+
+let playerCurrentTime = localStorage.getItem("videoplayer-current-time")
+console.log(playerCurrentTime);
+
+player.setCurrentTime(playerCurrentTime);
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
