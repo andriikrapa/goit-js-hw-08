9037d409895c049e96730b609264ca581d7f1fc8
andriikrapa <andrii.krapa@gmail.com>
2022-03-14 21:54:57 +0100

    Fix savePlayerCurrentTime function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,15 +6,13 @@ const player = new Vimeo.Player(iframe);
 player.on('timeupdate', throttle(savePlayerCurrentTime, 1000));
 
 function savePlayerCurrentTime(e) {
+    if (e.seconds === e.duration) {
+        localStorage.removeItem("videoplayer-current-time");
+        return;
+    }
     localStorage.setItem("videoplayer-current-time", e.seconds);
-    console.log(e.seconds);
 };
 
 let playerCurrentTime = localStorage.getItem("videoplayer-current-time")
-console.log(playerCurrentTime);
 
 player.setCurrentTime(playerCurrentTime);
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
