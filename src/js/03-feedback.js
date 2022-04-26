ed74e9d96a0fcc69cd2e1fc47dd8f85c0b3b6983
andriikrapa <andrii.krapa@gmail.com>
2022-04-26 20:31:27 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 200));
+
+populateFormData()
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(formData);
+    
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateFormData() {
+    const savedFormData = localStorage.getItem(STORAGE_KEY); 
+
+    if (savedFormData) {
+        const parsedSavedFromData = JSON.parse(savedFormData);
+
+        form.email.value = parsedSavedFromData.email || "";
+        form.message.value = parsedSavedFromData.message || "";
+    }
+};
